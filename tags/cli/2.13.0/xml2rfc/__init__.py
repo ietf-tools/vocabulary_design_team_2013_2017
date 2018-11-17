1c9d5d9e707332f29f1bb91c5eaa8be712c0b171
Henrik Levkowetz <henrik@levkowetz.com>
2018-11-17 13:25:13 +0000

    Creating new tag 'tags/cli/2.13.0' from trunk/cli  - Legacy-Id: 2741

---
+++ tags/cli/2.13.0/xml2rfc/__init__.py
@@ -0,0 +1,25 @@
+
+
+# Static values
+__version__  = '2.13.0'
+NAME         = 'xml2rfc'
+VERSION      = [ int(i) if i.isdigit() else i for i in __version__.split('.') ]
+CACHES       = ['/var/cache/xml2rfc', '~/.cache/xml2rfc']  # Ordered by priority
+CACHE_PREFIX = ''
+NET_SUBDIRS  = ['bibxml', 'bibxml2', 'bibxml3', 'bibxml4', 'bibxml5']
+
+from xml2rfc.parser import  XmlRfcError, CachingResolver, XmlRfcParser, XmlRfc
+
+from xml2rfc.writers import ( BaseRfcWriter, RawTextRfcWriter, PaginatedTextRfcWriter,
+	 HtmlRfcWriter, NroffRfcWriter, ExpandedXmlWriter, RfcWriterError,
+         V2v3XmlWriter, PrepToolWriter, TextWriter, HtmlWriter,
+     )
+
+# This defines what 'from xml2rfc import *' actually imports:
+__all__ = ['XmlRfcError', 'CachingResolver', 'XmlRfcParser', 'XmlRfc',
+           'BaseRfcWriter', 'RawTextRfcWriter', 'PaginatedTextRfcWriter',
+           'HtmlRfcWriter', 'NroffRfcWriter', 'ExpandedXmlWriter',
+           'RfcWriterError', 'V2v3XmlWriter', 'PrepToolWriter', 'TextWriter',
+           'HtmlWriter',
+       ]
+
